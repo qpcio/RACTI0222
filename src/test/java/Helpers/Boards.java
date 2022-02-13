de7bee9a813df831811d3862f03a868efe22d0c8
qpcio <qpcio@onet.eu>
2022-02-13 11:21:28 +0100

    day 2 mid

---
+++ src/test/java/Helpers/Boards.java
@@ -0,0 +1,62 @@
+package Helpers;
+
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+
+import static io.restassured.RestAssured.*;
+
+public class Boards {
+    private final String baseUri = "https://api.trello.com/1/";
+    private final String token = "6f94a562d1cfbb684255f657f6b16f98ace82a6945c4041e966c1eb9cea074df";
+    private final String key = "d8b4a9adf21cc384d7643ae35c69beb0";
+    private final String boardsAPIpath = "boards/";
+    private Response lastResponse;
+
+    public int getLastResponseStatusCode(){
+        return lastResponse.statusCode();
+    }
+
+    public String createBoard(String name) {
+        lastResponse = given()
+                .queryParam("name", name)
+                .when()
+                .post(boardsAPIpath);
+
+        return lastResponse.body()
+                .jsonPath()
+                .get("id");
+    }
+
+    public String getBoardName(String id) {
+        lastResponse =  when()
+                .get(boardsAPIpath + id);
+        return lastResponse
+                .body()
+                .jsonPath()
+                .get("name");
+    }
+
+    public void changeBoardName(String id, String newName) {
+        lastResponse = given()
+                .body("{\"name\": \"" + newName + "\"}")
+                .when()
+                .put(boardsAPIpath + id);
+    }
+
+    public void deleteBoard(String id) {
+        lastResponse = delete(boardsAPIpath + id);
+    }
+
+    @Deprecated
+    public int getBoardStatusCode(String id) {
+        return get(boardsAPIpath + id).statusCode();
+    }
+
+    public Boards() {
+        baseURI = this.baseUri;
+        requestSpecification = given().queryParam("token", token)
+                .queryParam("key", key)
+                .contentType(ContentType.JSON);
+    }
+
+}
---
+++ src/test/java/TrelloTests/BoardHelpersTests.java
@@ -0,0 +1,71 @@
+package TrelloTests;
+
+import Helpers.Boards;
+import io.restassured.path.json.exception.JsonPathException;
+import org.apache.http.HttpStatus;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class BoardHelpersTests {
+    Boards boards;
+
+    @Test
+    public void shouldCreateNewBoardWithGivenName() {
+        //given
+        String boardName = "Tralala";
+        //when
+        String boardID = boards.createBoard(boardName);
+        //then
+        Assertions.assertEquals(boardName, boards.getBoardName(boardID));
+    }
+
+    @Test
+    public void shouldCoverWholeCRUDTest() {
+        String boardName = "First Name";
+        String boardSecondName = "Second name";
+
+        String boardID = boards.createBoard(boardName);
+        Assertions.assertEquals(boardName, boards.getBoardName(boardID));
+        Assertions.assertEquals(HttpStatus.SC_OK, boards.getBoardStatusCode(boardID));
+
+        boards.changeBoardName(boardID, boardSecondName);
+        Assertions.assertEquals(boardSecondName, boards.getBoardName(boardID));
+        Assertions.assertEquals(HttpStatus.SC_OK, boards.getBoardStatusCode(boardID));
+
+        boards.deleteBoard(boardID);
+        Assertions.assertEquals(HttpStatus.SC_NOT_FOUND, boards.getBoardStatusCode(boardID));
+    }
+
+    @Test
+    public void shouldCoverWholeCRUDTestButBetter() {
+        String boardName = "First Name";
+        String boardSecondName = "Second name";
+
+        String boardID = boards.createBoard(boardName);
+        Assertions.assertEquals(HttpStatus.SC_OK, boards.getLastResponseStatusCode());
+        Assertions.assertEquals(boardName, boards.getBoardName(boardID));
+
+        boards.changeBoardName(boardID, boardSecondName);
+        Assertions.assertEquals(HttpStatus.SC_OK, boards.getLastResponseStatusCode());
+        Assertions.assertEquals(boardSecondName, boards.getBoardName(boardID));
+
+        boards.deleteBoard(boardID);
+        Assertions.assertEquals(HttpStatus.SC_OK, boards.getLastResponseStatusCode());
+        try {
+            boards.getBoardName(boardID);
+        } catch (JsonPathException e) {
+               //tu moze nic nie byc - chcemy tylko, zeby exception nie przelecial wyzej
+        }  finally {
+            Assertions.assertEquals(HttpStatus.SC_NOT_FOUND, boards.getLastResponseStatusCode());
+        }
+
+        //Assertions.assertEquals(HttpStatus.SC_NOT_FOUND,boards.getBoardStatusCode(boardID));
+    }
+
+
+    @BeforeEach
+    public void setUp() {
+        boards = new Boards();
+    }
+}
